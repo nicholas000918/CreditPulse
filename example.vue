965228c6579fb5c4d00e4d7020ddd1a937908723
nicholas000918 <111839487+nicholas000918@users.noreply.github.com>
2023-11-23 22:59:41 +0800

    Add files via upload

---
+++ example.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+TableComp
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import StartupTableVue from "~/components/dashboard/startupTable.vue";
+import TrendListVue from "~/components/dashboard/trendList.vue";
+import UpperTitleVue from "~/components/UpperTitle.vue";
+import TableComp from "~/components/TableComp.vue";
+
+export default {
+  name: "ExamplePage",
+  components: {
+    UpperTitleVue,
+    TrendListVue,
+    StartupTableVue,
+    TableComp,
+  },
+  layout: "dashboardLayout",
+  data() {
+    return {
+      // search: null
+    };
+  },
+  computed: {
+    ...mapGetters({}),
+  },
+  methods: {},
+};
+</script>
+
+<style scoped></style>
